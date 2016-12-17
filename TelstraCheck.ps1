dc013f74786efc6af1f77c76e088ca97637bec0a
Tas Gray <tas@axiomit.com.au>
2016-12-17 22:34:40 +1100

    First commit

---
+++ TelstraCheck.ps1
@@ -0,0 +1,37 @@
+﻿# --------------------------------------------------------------
+#
+# TelstraCheck.ps1
+#
+# --------------------------------------------------------------
+#
+# Using delegated administrator credentials, checks each 
+# tenant and determines if syndicated with Telstra. Results
+# are displayed in a gridview for easy filtering and sorting.
+# 
+# Note - applies to Australia only. Syndication in other 
+#        countries may mean something different.
+#
+# --------------------------------------------------------------
+
+# Import the modules
+Connect-MsolService -Credential $cred
+
+$tenantArray = @()
+
+# Get list of tenants & loop
+Get-MsolPartnerContract -All | ForEach { 
+    
+    $telstra = $false;
+
+    $tenantDetails = @{}
+    $tenantDetails.TenantName = [string]$_.DefaultDomainName
+    $tenantDetails.Telstra = $false
+    
+    Get-MsolAccountSku -TenantId $_.TenantId.Guid | Foreach { if($_.AccountName -eq "syndication-account") { $tenantDetails.Telstra = $true } }
+     
+    $object = New-Object -TypeName PSObject -Prop $tenantDetails
+    $tenantArray += $object
+
+}
+
+$tenantArray | Out-GridView
